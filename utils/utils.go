4e87e99f01e86d7eecd7b7fa6d65044a89334e23
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2025-05-09 15:24:47 -0400

    feat: flag for existing changelog file with validation

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/ammar-ahmed22/chlog/models"
 )
 
 func Eprintln(args ...any) {
@@ -11,4 +14,27 @@ func Eprintln(args ...any) {
 
 func Eprintf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format, args...)
+}
+
+func ParseAndValidateChangelogFile(path string) ([]models.ChangelogEntry, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		return nil, fmt.Errorf("File '%s' does not exist", path)
+	}
+	contents, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading file '%s': %v", path, err)
+	}
+
+	if len(contents) == 0 {
+		return []models.ChangelogEntry{}, nil
+	}
+
+	var changelog []models.ChangelogEntry
+	err = json.Unmarshal(contents, &changelog)
+	if err != nil {
+		return nil, fmt.Errorf("Changelog file '%s' is not valid JSON. See https://github.com/ammar-ahmed22/chlog#changelog-format for the expected format", path)
+	}
+
+	return changelog, nil
 }
